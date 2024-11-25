8982f72a14a813a73a08a2e0c43f3066af622078
luiskarlost25 <82845321+luiskarlost25@users.noreply.github.com>
2024-11-25 13:33:10 -0500

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+
+
+
+module.exports = {
+    publicPath:"./",
+
+    devServer: {
+      host: '0.0.0.0', // Permite conexiones desde cualquier IP
+      port: 8080,      // Puerto por defecto (puedes cambiarlo)
+      allowedHosts: 'all', // Permite cualquier dominio
+    },
+  };
+  
